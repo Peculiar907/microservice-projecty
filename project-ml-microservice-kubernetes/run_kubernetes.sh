8213d9e8bd23ace03dce16d7de477d1574da00f8
peculiar907 <peculiar907@gmail.com>
2022-07-22 04:12:13 +0100

    project

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath="peculiar907/flaskapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run flaskapi --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskapi 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=peculiar907/flaskapi
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag flaskapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
